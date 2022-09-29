aa9f55ef67e113619fe495cf484967a09a921a96
Cavid Bashirov <bashirovcavid93gmail.com>
2022-09-29 16:29:08 +0400

    created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupComponent } from './components/group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupComponent,
   ],
   imports: [
     BrowserModule,
@@ -14,5 +16,6 @@ import { AppComponent } from './app.component';
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
